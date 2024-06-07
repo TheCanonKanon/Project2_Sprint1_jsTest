eba2a91073f3e75806f7f2ebe694c70f53bdc657
TheCanonKanon <robert.wollny3@gmail.com>
2024-06-07 12:57:23 +0200

    Solved Exercise 1-6 made it more visible in console which is what exercise

--- main.js
+++ main.js
@@ -1,1 +1,63 @@
-console.log("Hello World");
+//Exercise 1
+console.log("Exercise 1");
+const myName = "Robert";
+console.log(myName);
+
+//Exercise 2
+console.log("-------------");
+console.log("Exercise 2");
+const age = 33;
+console.log(age);
+
+//Exercise 3
+console.log("-------------");
+console.log("Exercise 3");
+const juliaAge = 32;
+const ageDiff = age - juliaAge;
+console.log(ageDiff);
+
+//Exercise 4
+console.log("-------------");
+console.log("Exercise 4");
+if (age > 21) {
+    console.log("You are older than 21");
+} else {
+    console.log("You are not older than 21");
+}
+
+//Exercise 5
+console.log("-------------");
+console.log("Exercise 5");
+if (age > juliaAge) {
+    console.log("You are older than Julia");
+} else if (age === juliaAge) {
+    console.log("You are the same age as Julia");
+} else {
+    console.log("You are younger than Julia");
+}
+
+//Exercise 6
+console.log("-------------");
+console.log("Exercise 6");
+const classArray = ["Hadi","Stefan","Robert"];
+console.log(classArray[0]);
+console.log(classArray[classArray.length-1]);
+for (let i = 0; i < classArray.length; i++) {
+    console.log(classArray[i]);
+}
+
+//Exercise 7
+console.log("-------------");
+console.log("Exercise 7");
+
+//Exercise 8
+console.log("-------------");
+console.log("Exercise 8");
+
+//Exercise 9
+console.log("-------------");
+console.log("Exercise 9");
+
+//Exercise 10
+console.log("-------------");
+console.log("Exercise 10");
