c3ffda15e8ee5cc03e055b7360529f54b7b82019
TheCanonKanon <robert.wollny3@gmail.com>
2024-06-07 14:18:29 +0200

    solved Exercise 7-10

--- main.js
+++ main.js
@@ -39,25 +39,59 @@ if (age > juliaAge) {
 //Exercise 6
 console.log("-------------");
 console.log("Exercise 6");
-const classArray = ["Hadi","Stefan","Robert"];
-console.log(classArray[0]);
-console.log(classArray[classArray.length-1]);
-for (let i = 0; i < classArray.length; i++) {
-    console.log(classArray[i]);
+const classNameArray = ["Hadi","Stefan","Robert"];
+console.log(classNameArray[0]);
+console.log(classNameArray[classNameArray.length-1]);
+for (let i = 0; i < classNameArray.length; i++) {
+    console.log(classNameArray[i]);
 }
 
 //Exercise 7
 console.log("-------------");
 console.log("Exercise 7");
+const classAgeArray = [24,33,40];
+
+for (let i = 0; i < classAgeArray.length; i++) {
+    console.log(classAgeArray[i]);
+}
+
+let i = 0;
+while (i < classAgeArray.length) {
+    if (classAgeArray[i] % 2 === 0) {
+        console.log(classAgeArray[i]);
+    }
+    i++;
+}
 
 //Exercise 8
 console.log("-------------");
 console.log("Exercise 8");
+let classAgeSum = 0;
+for (let i = 0; i < classAgeArray.length; i++) {
+    classAgeSum = classAgeSum + classAgeArray[i]; 
+}
+console.log(classAgeSum);
 
 //Exercise 9
 console.log("-------------");
 console.log("Exercise 9");
+const NumberArray = [1, 2, 8, 3, 2];
+let NumberArraySum = 0;
+for (let i = 0; i < NumberArray.length; i++) {
+    if (NumberArray[i] % 2 === 0) {
+        NumberArraySum = NumberArray[i] + NumberArraySum;
+    }
+}
+console.log(NumberArraySum);
 
 //Exercise 10
 console.log("-------------");
 console.log("Exercise 10");
+const NumberArray2 = [1, 2, 8, 3, 2, 3, 4];
+let NumberArray2Sum = 0;
+for (let i = 0; i < NumberArray2.length; i++) {
+    if (i % 2 === 0 && i != 0) {
+        NumberArray2Sum = NumberArray2[i] + NumberArray2Sum;
+    }
+}
+console.log(NumberArray2Sum);
